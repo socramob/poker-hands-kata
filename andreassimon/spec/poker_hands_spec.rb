cdad829b0c0d4c3f7b4a433a2ba49eb5b7b06c3a
Andreas Simon <mail@andreas-simon.info>
2012-11-02 20:07:49 +0100

    GREEN: PokerCard#value can be 9

--- andreassimon/spec/poker_hands_spec.rb
+++ andreassimon/spec/poker_hands_spec.rb
@@ -38,6 +38,13 @@ class PokerCard
   SPADES = 'S'
 
   VALUE_2 = '2'
+  VALUE_3 = '3'
+  VALUE_4 = '4'
+  VALUE_5 = '5'
+  VALUE_6 = '6'
+  VALUE_7 = '7'
+  VALUE_8 = '8'
+  VALUE_9 = '9'
 
   def initialize(suit_and_value)
     @value, @suit = suit_and_value[0], suit_and_value[1]
